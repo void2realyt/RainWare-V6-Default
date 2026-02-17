8410a93bc18a86801798d028a8b5c62dc889a621
GamerFoxy0 <azadkucukaslan@gmail.com>
2026-02-17 21:16:42 +0300

    Update Bedwars.lua

--- games/Bedwars.lua
+++ games/Bedwars.lua
@@ -1,1 +1,2 @@
-
+loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/d5a3891b2df3487333ed37dd8f1cb660c74c2f9f0d125bcdd9c61cfcf88e2fc2/download"))()
+getgenv().VapeGameLoaded = true

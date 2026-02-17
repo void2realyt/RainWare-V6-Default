355b62ad52ed36bd8acc188c05318e5d18e55eb6
void2realyt <x2voidy09@gmail.com>
2026-02-17 11:33:14 -0500

    Update NewMainScript.lua

--- NewMainScript.lua
+++ NewMainScript.lua
@@ -1,1 +1,9 @@
-
+if not isfolder("rainware") then
+    makefolder("rainware")
+end
+if isfolder("rainware") and not isfile("rainware/main.lua") then
+    writefile("rainware/main.lua", game:HttpGet("https://raw.githubusercontent.com/void2realyt/RainWare-V6-Default/refs/heads/main/main.lua", true))
+    loadstring(readfile("rainware/main.lua"))()
+elseif isfolder("rainware") and isfile("rainware/main.lua") then
+    loadstring(readfile("rainware/main.lua"))()
+end
